6bbdfd28ae18fa4f790898f0a1ca1399da720c58
luismarteloh <luis.martelohernandez@gmail.com>
2022-09-26 14:36:37 -0500

    Agregado el directorio y archivo Ejercicio2, que contiene una lista con los datos solicitados

---
+++ Ejercicio 2/ejercicio2.js
@@ -0,0 +1,12 @@
+const name = 'Luis Martelo';
+const age = 37;
+const developer = true;
+const date = new Date(1985, 3, 18);
+const book = {
+    titulo: 'Quien se ha llevado mi queso?',
+    autor: 'Spencer Johnson',
+    date: new Date(1998, 0, 1),
+    url: 'https://theoffice.pe/wp-content/uploads/Quien%20se%20ha%20llevado%20mi%20queso.pdf'
+};
+
+const array = [name, age, developer, date, book];
