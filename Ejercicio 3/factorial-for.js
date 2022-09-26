b7aa03afc065492d2f6c0fb0f407bef9e20390f0
luismarteloh <luis.martelohernandez@gmail.com>
2022-09-26 16:06:09 -0500

    Agregado el directorio Ejercicio 3, con los 3 respectivos archivos bucles solicitados

---
+++ Ejercicio 3/factorial-for.js
@@ -0,0 +1,10 @@
+let factorial = 1
+
+for (let i = 1; i < 10; i++) {
+    
+    factorial = factorial * i
+    console.log(factorial)
+    
+}
+
+console.log('El factorial de 10 es: ', factorial)
---
+++ Ejercicio 3/factorial-while.js
@@ -0,0 +1,9 @@
+let factorial = 1
+let contador = 1
+
+while (contador != 10) {
+    factorial = factorial * contador;
+    contador++;
+}
+
+console.log('El factorial de 10 es: ', factorial)
