fd484a8a3215f48f6b456f6c9689f2f1e8985c49
luismarteloh <luis.martelohernandez@gmail.com>
2022-10-14 07:44:03 -0500

    corregido el catch

--- Ejercicio 9/index.js
+++ Ejercicio 9/index.js
@@ -2,15 +2,16 @@ const logger = require("./logger");
 
 function error() {
     throw new Error("Esta funcion lanza siempre un error")
-    
 }
 
 try {
     error();
-    console.log("funcion ejecutada correctamente")
+    console.log("funcion ejecutada correctamente");
+
 } catch (e) {
-    logger.error("La funcion se ejecutó pero presentó un error");
-    logger.error("La funcion se ejecutó pero presentó un error", e);
+    logger.error(`El error capturado es: ${e}`); //queda en el registro
+    console.log(`${e}`);  //se muestra en consola
+
 } finally {
     logger.info("la funcion 'error' se ejecutó");
 }
--- Ejercicio 9/logger/index.js
+++ Ejercicio 9/logger/index.js
@@ -4,14 +4,13 @@ const logger = winston.createLogger({
   level: 'silly', //nivel mas alto para registrar errores
   format: winston.format.json(),
   //https://www.npmjs.com/package/winston     instruction about winston
-  //defaultMeta: { service: 'user-service' },
+  defaultMeta: { service: 'user-service' },
   transports: [
     //
     // - Write all logs with importance level of `error` or less to `error.log`
     // - Write all logs with importance level of `info` or less to `combined.log`
     //
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
   ],
 });
 
