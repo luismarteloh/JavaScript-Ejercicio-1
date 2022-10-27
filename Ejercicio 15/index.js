24ea4872fcee6ad1ea50446526b2843babc6aed6
luismarteloh <luis.martelohernandez@gmail.com>
2022-10-27 15:42:19 -0500

    Carpetas Sesion 15 y Ejerccio 15 creadas esta ultima con sus archivos y elementos requeridos en la actividad

---
+++ Ejercicio 15/index.js
@@ -0,0 +1,13 @@
+// const nombre = "Luis"
+// const apellido = "Martelo"
+// const obj = {
+//     nombre: nombre,
+//     apellido: apellido 
+// }
+
+// sessionStorage.setItem("obj",obj)
+// // console.log(JSON.parse(sessionStorage.getItem("obj")))
+// localStorage.setItem("obj", obj)
+
+// // document.cookie = obj
+// document.cookie =  `nombreCaducidad=${obj};expires=` + new Date(2022, 9, 27, 15, 38).toUTCString()
